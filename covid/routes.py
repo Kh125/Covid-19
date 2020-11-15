d504b248e65df2fae3f3424c667be017eda0b0da
Khant Hmuu <khanthmu152uit@gmail.com>
2020-11-15 12:59:45 +0000

    add world wide cases and dashboard ui

--- covid/routes.py
+++ covid/routes.py
@@ -1,22 +1,35 @@
 from flask import render_template, request
-import requests
 from covid import app
+import requests
+import os
+import csv
 
-url = "https://covid-193.p.rapidapi.com/statistics"
+urll = "https://covid-193.p.rapidapi.com/"
 headers = {
-      'x-rapidapi-key': "bce42a815emsh56bee14b9198293p194e08jsne213c82bc0d4",
+      'x-rapidapi-key': os.getenv('x-rapidapi-key'),
       'x-rapidapi-host': "covid-193.p.rapidapi.com"
+      }
+
+url_world = "https://worldometers.p.rapidapi.com/api/coronavirus/world"
+
+headers_world = {
+      'x-rapidapi-key': os.getenv('x-rapidapi-key'),
+      'x-rapidapi-host': "worldometers.p.rapidapi.com"
       }
 
 @app.route('/country',methods=['GET','POST'])
 def country():
+  url = urll+'statistics'
   country = "Myanmar"
   if request.method == 'POST':
-    country = request.form.get('city')
+    if request.form.get('city').lower() == 'south-korea':
+      country = 's-korea'
+    else:
+      country = request.form.get('city')
   querystring = {"country":country}
   response = requests.request("GET", url, headers=headers, params=querystring)
   req = response.json()
-  print(req)
+  # print(req)
 
   if req['results'] != 0:
     country_data = {
@@ -36,14 +49,38 @@ def country():
     }
   else:
     country_data = {
+      'country':req['parameters']['country'],
       'result':req['results']
     }
 
-  print(country_data)
+  # print(country_data)
+  with open('flag.csv','r')as rf:
+    csv_reader = csv.reader(rf)
+    next(csv_reader)
+    for i in csv_reader:
+      if country_data['country'] == i[0]:
+        # print(i[2])
+        country_data['svg'] = i[2]
   return render_template('index.html',data = country_data)
-
 
 @app.route('/dashboard',methods=['GET','POST'])
 @app.route('/',methods=['GET','POST'])
-def index():
-  return render_template('dashboard.html')
+def index():  
+
+  response = requests.request("GET", url_world, headers=headers_world)
+  req = response.json()  
+  world = {
+    'active':req['data']['Active Cases'],
+    'critical':req['data']['Critical'],
+    'new_case':req['data']['New Cases'],
+    'new_death':req['data']['New Deaths'],
+    'new_recover':req['data']['New Recovered'],
+    'total_case':req['data']['Total Cases'],
+    'total_death':req['data']['Total Deaths'],
+    'total_recover':req['data']['Total Recovered'],
+    'total_test':req['data']['Total Tests'],
+    'updated':req['last_update']
+  }
+  print(world)
+  return render_template('dashboard.html', data = world)
+
